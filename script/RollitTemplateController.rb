48a2e44322c19a30f0bfd250aa2192fcfcaa793f
Eric Sean Conner <conner@fh-worms.de>
2011-06-09 11:23:49 +0200

    Add rendering of units, videos, exercises, notes, etc. Rename scripts.

--- script/RollitTemplateController.rb
+++ script/RollitTemplateController.rb
@@ -4,6 +4,7 @@ require 'rdiscount'
 
 require File.dirname(__FILE__) + '/rollit_common'
 
+# Check out Crafting Rails Applications by Jose Valim (Pragmatic Programmers)
 ActionView::Template.register_template_handler :md, lambda { |template| "RDiscount.new(#{template.source.inspect}).to_html" }
 ActionView::Template.register_template_handler :txtl, lambda { |template| "RedCloth.new(#{template.source.inspect}, [:hard_breaks]).to_html" }
 
@@ -13,16 +14,68 @@ class RollitTemplateController < AbstractController::Base
   include AbstractController::Helpers
   include AbstractController::AssetPaths
 
-  self.view_paths = DYNAMIC_OUTPUT_TEMPLATE_DIR
+  self.view_paths = [DYNAMIC_OUTPUT_TEMPLATE_DIR, SECTIONS_DIR]
   
+  helper_method :render_navigation
   helper_method :render_sections
+  helper_method :render_units
+  helper_method :render_summary_images
+  helper_method :render_notes_for_unit
+  helper_method :render_exercises_for_unit
+  
+  def render_navigation
+    navigation_elements = []
+    section_entities.each do |section_id, section_name|
+      unit_nav_elements = []
+      unit_entities_in_section(section_id).each do |unit_id, unit_name|
+        unit_nav_elements << [unit_id, unit_name]
+      end
+      navigation_elements << [section_id, section_name, unit_nav_elements]      
+    end
+    self.render template: 'navigation', locals: {nav_elements: navigation_elements}
+  end
   
   def render_sections
     sections = ActionView::OutputBuffer.new
     section_entities.each do |key, value|
-      sections << (self.render template: 'section', locals: {section_number: key, section_name: value})
+      sections << (self.render template: 'section', locals: {section_id: key, section_name: value})
     end
     sections
   end
+  
+  def render_units(section_id)
+    units = ActionView::OutputBuffer.new
+    unit_entities_in_section(section_id).each do |unit_id, unit_name|
+      video_base = output_video_base_name_in_unit(section_id, unit_id)
+      units << (self.render template: 'unit', locals: {section_id: section_id, unit_id: unit_id, unit_name: unit_name, video_base: video_base})
+    end
+    units
+  end
+  
+  def render_summary_images(section_id, unit_id)
+    images = ActionView::OutputBuffer.new
+    output_summary_images_in_unit(section_id, unit_id).each do |image_name|
+      images << (self.render template: 'image', locals: {image_name: "#{IMAGE_OUTPUT_DIR}/#{image_name}", 
+                                                        thumbnail_image_name: "#{THUMBNAIL_IMAGE_OUTPUT_DIR}/#{image_name}"})
+    end
+    images     
+  end
+  
+  def content_template_path(section_id, unit_id)
+    section_name = section_entities[section_id]
+    section = entity(section_id, section_name)
+    unit_name = unit_entities_in_section(section_id)[unit_id]
+    unit = entity(unit_id, unit_name)
+    "#{section}/units/#{unit}"
+  end
+  
+  def render_notes_for_unit(section_id, unit_id)
+    ActionView::OutputBuffer.new(self.render template: "#{content_template_path(section_id, unit_id)}/notes")    
+  end
+  
+  def render_exercises_for_unit(section_id, unit_id)
+    ActionView::OutputBuffer.new(self.render template: "#{content_template_path(section_id, unit_id)}/exercises")
+  end
+
   
 end
--- script/rollit_common.rb
+++ script/rollit_common.rb
@@ -3,6 +3,9 @@ SCRIPT_DIR = File.dirname(__FILE__)
 CONTENT_DIR = "#{SCRIPT_DIR}/../content"
 SECTIONS_DIR = "#{CONTENT_DIR}/sections"
 OUTPUT_DIR = "#{SCRIPT_DIR}/../output"
+IMAGE_OUTPUT_DIR = "#{OUTPUT_DIR}/_/images"
+THUMBNAIL_IMAGE_OUTPUT_DIR = "#{OUTPUT_DIR}/_/images/thumbnails"
+VIDEO_OUTPUT_DIR = "#{OUTPUT_DIR}/_/video"
 
 TEMPLATE_DIR = "#{SCRIPT_DIR}/../templates"
 
@@ -22,6 +25,7 @@ end
 def entities_in_dir(dir, entity_type)
   entities_hash = {}
   dir.entries.each do |entry|
+    next if entry[0] == '.'
     entity_id, entity_name = entry.split('_', 2)
     entity_id = entity_id.to_i
     next unless entity_id && entity_name
@@ -35,3 +39,47 @@ def section_entities
   sections_dir = Dir.new SECTIONS_DIR
   entities_in_dir(sections_dir, 'section')  
 end
+
+def entity(entity_id, entity_name)
+  "#{entity_id}_#{entity_name}"
+end
+
+def section_path(section_id, section_name = nil)
+  section_name = section_entities[section_id] unless section_name
+  "#{SECTIONS_DIR}/#{entity(section_id, section_name)}"  
+end
+
+def unit_entities_in_section(section_id)  
+  units_dir = Dir.new("#{section_path(section_id)}/units")
+  entities_in_dir(units_dir, 'unit')
+end
+
+def unit_path(section_id, unit_id, unit_name = nil)
+  unit_name = unit_entities_in_section(section_id)[unit_id] unless unit_name
+  "#{section_path(section_id)}/units/#{entity(unit_id, unit_name)}"
+end
+
+def summary_images_in_unit(section_id, unit_id)
+  images_dir = Dir.new("#{unit_path(section_id, unit_id)}/summary_images")
+  images_dir.entries.find_all { |entry| entry[0] != '.' }
+end
+
+def output_summary_images_in_unit(section_id, unit_id)
+  images_dir = Dir.new(IMAGE_OUTPUT_DIR)
+  prefix = "#{section_id}_#{unit_id}_"
+  images_dir.entries.find_all { |entry| entry =~ Regexp.new("^#{prefix}") }
+end
+
+def video_in_unit(section_id, unit_id)
+  video_dir = Dir.new("#{unit_path(section_id, unit_id)}/video")
+  video_dir.entries.find_all { |entry| entry[0] != '.' }
+end
+
+def output_video_base_name_in_unit(section_id, unit_id)
+  video_dir = Dir.new(VIDEO_OUTPUT_DIR + "/#{section_id}_#{unit_id}/")
+  base_name = video_dir.entries.find { |entry| entry =~ Regexp.new("mov$") }
+  base_name ? base_name[0...-4] : nil
+end
+    
+
+
